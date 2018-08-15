051e0032a509ffc5caf7a2270bacd378f4cb4adc
NirMekin <nir.mekin@gmail.com>
2018-08-15 19:14:35 +0300

    update search service

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 
 import { Movie , MovieMetadat } from './movie';
 import { Observable } from 'rxjs/internal/Observable';
+import { map } from 'rxjs/internal/operators/map';
 
 
 @Injectable({
@@ -13,21 +14,21 @@ export class SearchService {
 
   constructor(private http : HttpClient) { }
 
-  
-
-  // getMoviesByGener(gener): Promise<Movie[]>{
-  //   return this.http.get(`../../assets/movie_api/movies.json`)
-  //           .toPromise()
-  //           .then(response => response.json().movies.filter( i => i.gener = gener) as Movie[])
-  // }
+ 
 
   getMoviesByGener(gener): Observable<MovieMetadat[]>{
-    return this.http.get<MovieMetadat[]>("../../assets/movie_api/movies-metadata.json");
+    return this.http.get<MovieMetadat[]>("../../assets/movie_api/movies-metadata.json")
+            .pipe(
+              map( movie => movie.filter( i => i.geners.includes(gener) ))
+            );
   }
 
 
   getMoviesByName(name): Observable<MovieMetadat[]>{
-    return this.http.get<MovieMetadat[]>("../../assets/movie_api/movies-metadata.json");
+    return this.http.get<MovieMetadat[]>("../../assets/movie_api/movies-metadata.json")
+           .pipe(
+             map( data => data.filter(i => i.name.indexOf(name) !== -1 ))
+           )
   }
   
 }
